7d7f78aa2a432ee72c819239e4525af60fe08799
paulgitcode <paul.temo@live.com>
2016-10-16 19:00:59 -0400

    Paul Update Files
    Updating project to include all files.

--- src/titleJLabel.java
+++ src/titleJLabel.java
@@ -1,6 +1,17 @@
+
 import java.awt.Font;
 import javax.swing.JLabel;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Paul
+ */
 public class titleJLabel extends JLabel
 {
     
