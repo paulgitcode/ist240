236e4a170781234f1d30cf9aaac10282d3dbe195
bradreamer <bradreamer@gmail.com>
2016-10-16 10:09:59 -0400

    Create titleJLabel.java

---
+++ titleJLabel.java
@@ -0,0 +1,19 @@
+import java.awt.Font;
+import javax.swing.JLabel;
+
+public class titleJLabel extends JLabel
+{
+    
+    String text;
+    
+    public titleJLabel(String informedText)
+    {
+         super(); 
+         text = informedText;
+         setFont(new Font("Serif", Font.BOLD, 16));
+         setText(text);
+    
+    }
+    
+    
+}
