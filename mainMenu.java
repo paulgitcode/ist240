8a0a8cd5f385944dddf60e09062e7ffd80777087
bradreamer <bradreamer@gmail.com>
2016-10-16 10:07:06 -0400

    Create mainMenu.java

---
+++ mainMenu.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+
+
+public class mainMenu extends JPanel
+{
+    JButton b1;
+    JButton b2;
+    JButton b3;
+    JButton b4;
+    JButton b5;
+    titleJLabel la1;
+    
+    
+    public mainMenu()
+    {
+        super();
+        
+
+        
+        setBackground(Color.gray);
+        
+       
+        
+        b1 = new JButton("About");
+        b2 = new JButton("Team Members");
+        b3 = new JButton("Options");
+        b4 = new JButton("How to Play");
+        b5 = new JButton("Start Game");
+        la1 = new titleJLabel("The Side Scroller");
+        
+        
+        add(la1);
+        add(b1);
+        add(b2);
+        add(b4);
+        add(b3);
+        add(b5);
+        
+        
+    }
+}
